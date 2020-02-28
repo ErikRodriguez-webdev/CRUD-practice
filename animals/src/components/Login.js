d02c2605fa91b1dbbe1a204ff130ae19b32b62cc
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-27 16:39:53 -0800

    Add Login functionality and utils
    This was to return a login token with authentication axios post call and use private routes.

--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 export default function Login(props) {
-    // How can we log in? What do we need to do?
+  // How can we log in? What do we need to do?
+  const [login, setLogin] = useState({
+    username: "",
+    password: ""
+  });
 
-    return (
-        <div>
-            <h1>Welcome to the Safari App!</h1>
-            <h2>I can't show you more until you log in. Please build out a login.</h2>
-        </div>
-    )
+  const handleChange = (event) => {
+    setLogin({ ...login, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axiosWithAuth()
+      .post("/login", login)
+      .then((response) => {
+        console.log(response);
+        window.localStorage.setItem("token", response.data.payload);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+
+    setLogin({
+      username: "",
+      password: ""
+    });
+  };
+
+  return (
+    <div>
+      <h1>Welcome to the Safari App!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            name="username"
+            value={login.username}
+            onChange={handleChange}
+            placeholder="jerrySpringer123"
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            name="password"
+            value={login.password}
+            onChange={handleChange}
+            placeholder="abc123!@#"
+          />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
